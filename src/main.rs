26a661b40e29429275e680771aa5e58affdf08a7
pauljoohyunkim <pauljoohyunkim@gmail.com>
2026-08-26 16:46:52 +0900

    Command line argument parsing.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use std::env;
 mod cvtree;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
     let ctree: cvtree::CVTree = cvtree::CVTree::new(2, cvtree::CVType::ConsonantFormer);
 
     println!("{ctree:?}");
